5361ff830d678d987b3ecda0e5bcbc37ee293168
MsCelia <msceliamyburgh@gmail.com>
2021-12-16 15:57:58 +0200

    Remove array No test found

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayOne) {
+    let removeTest1 = arrayOne.pop();
+    return removeTest1
 };
 
 // Do not edit below this line
